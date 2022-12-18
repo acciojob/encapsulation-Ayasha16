7449c28d18b8145c30402d0e61b9e28af663c97f
Ayasha <khanayasha190@gmail.com>
2022-12-18 17:50:38 +0530

    Encapsulation assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+        //Error: variable obj of type com.driver.RWOnly(cannot find symbol)
+        obj.setName("Ayasha");
+        System.out.println(obj.getName());
+    }
   
 }
